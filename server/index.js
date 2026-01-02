d341c217317cc641288147a4b0134fcc2ffafa93
abdelrahmanfathyradwan <afat7i2005@gmail.com>
2026-01-02 04:28:18 +0200

    g

--- server/index.js
+++ server/index.js
@@ -31,7 +31,10 @@ app.use(async (req, res, next) => {
     }
 });
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
